ca2c26eaaee602ed918c70e7cd68710bee2e985c
Arpit6367 <arpit562005@gmail.com>
2025-04-04 18:02:08 +0530

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  content: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      display: []
+      // 'display' is not something you usually extend directly like this
     },
   },
   variants: {
